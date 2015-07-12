7ff2b3d7ecdef2c5a3d656d851326660820211c3
UserX <user_x@riseup.net>
2015-07-11 21:31:30 -0300

    modified:   virgulino.rb

--- virgulino.rb
+++ virgulino.rb
@@ -4,28 +4,44 @@ class String
     def green; "\033[32m#{self}\033[0m" end
 end
 
+
 def splash()
     puts"*****************************************************".green
     puts"*         __                    __ __               *".green
     puts"* .--.--.|__|.----.-----.--.--.|  |__|.-----.-----. *".green
     puts"* |  |  ||  ||   _|  _  |  |  ||  |  ||     |  _  | *".green
-    puts"*  \\___/ |__||__| |___  |_____||__|__||__|__|_____| *".green
+    puts"* \\___/ |__||__| |___  |_____||__|__||__|__|_____| *".green
     puts"*                 |_____|                           *".green
     puts"*                                                   *".green
     puts"* Virgulino Ver. 0.1                                *".green
     puts"* coded by n3k00n3                                  *".green
     puts"* LampiaoSEC - Security Research Group              *".green
-    puts"* #lampiaosec at freenode                           *".green
+    puts"* #lampiaosec at freenode                           *\n".green
     puts"*****************************************************\n".green
 end
-splash()
 
-puts "What do you wanna do?".green
-puts "[1 - to encrypt]".green
-puts "[2 - to decrypt]".green
-answer = gets.chomp
+def bin_to_s(str)
+    newstr = ""
+    counter = 0
+    ascii = ""
+    
+    final = ""
+    str.each_char do |x| 
+        newstr += x
+        counter += 1
+        if counter == 7
+            ascii = newstr.to_i (2)
+            final += ascii.chr
+            counter = 0
+            ascii = ""
+            newstr = ""
+        end
+    end
 
-if answer == "1"
+    final 
+end
+
+def encrypt
     puts "Write whatever text you want to encrypt and press enter.".green
     $text = gets.chomp
     $text2= ""
@@ -38,7 +54,9 @@ if answer == "1"
     f.write($text2)
     f.close
     puts "Your file is done! It`s name is virgulino.".green
-elsif answer == "2"
+end
+
+def decrypt
     exist=false
     while exist != true
         puts "Enter the encrypted filename:".green
@@ -48,7 +66,11 @@ elsif answer == "2"
             stegofile = File.read("#{$file}")
 
             #convert spaces/tabs to bin
-    	    stegofile.gsub!(/[\t]/, "\t" => "1", " " => "0")
+    	    stegofile.gsub!(/[\t]/ , "\t" => "1")
+            stegofile.gsub!(/[ ]/ , "0")
+
+            stegofile = bin_to_s(stegofile)
+
             puts "What should be the output filename?".green
             name_file = gets.chomp
         	f = File.new("#{name_file}", "w")
@@ -66,3 +88,26 @@ elsif answer == "2"
         end
     end
 end
+
+def menu
+
+    puts "What do you wanna do?".green
+    puts "[1 - to encrypt]".green
+    puts "[2 - to decrypt]".green
+    answer = gets.chomp
+    answer
+end
+
+
+def main
+
+    splash()
+    if menu() == "1"
+        encrypt()
+    else 
+        decrypt()
+    end 
+
+end
+
+main()
